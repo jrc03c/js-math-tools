d09c6b007669192b39b79373780b56c9d71f5652
jrc03c <jrc03c@pm.me>
2020-02-20 09:46:52 -0600

    ...

---
+++ index.js
@@ -0,0 +1,462 @@
+let primaryColor = "#3273dc"
+let secondaryColor = "#ff3860"
+
+Array.prototype.asyncForEach = async function(fn){
+  for (let i=0; i<this.length; i++) await fn(this[i], i, this)
+  return this
+}
+
+Array.prototype.alphaSort = function(key){
+  return this.sort(function(a, b){
+    if (key){
+      if (a[key] < b[key]) return -1
+      if (a[key] > b[key]) return 1
+      return 0
+    } else {
+      if (a < b) return -1
+      if (a > b) return 1
+      return 0
+    }
+  })
+}
+
+function print(x){
+  return console.log(x)
+}
+
+function isArray(obj){
+  return obj.push ? true : false
+}
+
+function ones(n){
+  return range(0, n).map(v => 1)
+}
+
+function zeros(n){
+  return range(0, n).map(v => 0)
+}
+
+function pause(ms){
+  return new Promise(function(resolve, reject){
+    try {
+      return setTimeout(resolve, ms)
+    } catch(e){
+      return reject(e)
+    }
+  })
+}
+
+function downloadCanvas(canvas, filename){
+  let a = document.createElement("a")
+  a.href = canvas.toDataURL()
+  a.download = filename
+  a.dispatchEvent(new MouseEvent("click"))
+}
+
+function round(x){
+  if (isArray(x)) return x.map(round)
+  return Math.round(x)
+}
+
+function floor(x){
+  if (isArray(x)) return x.map(floor)
+  return Math.floor(x)
+}
+
+function ceil(x){
+  if (isArray(x)) return x.map(ceil)
+  return Math.ceil(x)
+}
+
+function abs(x){
+  if (isArray(x)) return x.map(abs)
+  return Math.abs(x)
+}
+
+function sin(x){
+  if (isArray(x)) return x.map(sin)
+  return Math.sin(x)
+}
+
+function cos(x){
+  if (isArray(x)) return x.map(cos)
+  return Math.cos(x)
+}
+
+function tan(x){
+  if (isArray(x)) return x.map(tan)
+  return Math.tan(x)
+}
+
+function clamp(x, a, b){
+  if (isArray(x)) return x.map(v => clamp(v, a, b))
+  if (x < a) return a
+  if (x > b) return b
+  return x
+}
+
+function normalize(arr){
+  let arrMin = min(arr)
+  let arrMax = max(arr)
+  let arrRange = arrMax - arrMin
+  return arr.map(v => (v - arrMin) / arrRange)
+}
+
+function sqrt(x){
+  if (isArray(x)) return x.map(sqrt)
+  return Math.sqrt(x)
+}
+
+function pow(x, p){
+  if (isArray(x)) return x.map(v => pow(v, p))
+  return Math.pow(x, p)
+}
+
+function sum(arr){
+  let out = 0
+  arr.forEach(v => out += v)
+  return out
+}
+
+function mean(arr){
+  return sum(arr) / arr.length
+}
+
+function std(arr){
+  let m = mean(arr)
+  let out = 0
+  arr.forEach(x => out += Math.pow(x - m, 2))
+  return Math.sqrt(out / arr.length)
+}
+
+function variance(arr){
+  return pow(std(arr), 2)
+}
+
+function cohensd(arr1, arr2){
+  let m1 = mean(arr1)
+  let m2 = mean(arr2)
+  let s = sqrt((variance(arr1) + variance(arr2)) / 2)
+  return (m1 - m2) / s
+}
+
+function max(arr){
+  let out
+
+  arr.forEach(function(x){
+    if (out === undefined || x > out){
+      out = x
+    }
+  })
+
+  return out
+}
+
+function min(arr){
+  let out
+
+  arr.forEach(function(x){
+    if (out === undefined || x < out){
+      out = x
+    }
+  })
+
+  return out
+}
+
+function cov(x, y){
+  let mx = mean(x)
+  let my = mean(y)
+  let out = 0
+  for (let i=0; i<x.length; i++) out += (x[i] - mx) * (y[i] - my)
+  return out / x.length
+}
+
+function correl(x, y){
+  return cov(x, y) / (std(x) * std(y))
+}
+
+function normal(){
+  let u1 = Math.random()
+  let u2 = Math.random()
+  return Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2)
+}
+
+function range(a, b, step=1){
+  let out = []
+  for (let i=a; i<b; i+=step) out.push(i)
+  return out
+}
+
+function map(x, a, b, c, d){
+  return (d - c) * (x - a) / (b - a) + c
+}
+
+function lerp(a, b, f){
+  return f * (b - a) + a
+}
+
+function Plot(canvas){
+  let self = this
+
+  let xmin = -1
+  let xmax = 1
+  let ymin = -1
+  let ymax = 1
+  let fillColor = "black"
+  let strokeColor = "black"
+  let dotSize = 5
+  let lineThickness = 1
+  let axesAreVisible = true
+  let textStyle = {
+    family: "monospace",
+    size: 12,
+    alignment: "center",
+    baseline: "middle",
+    isBold: false,
+    isItalicized: false,
+    lineHeight: 14,
+    color: "black",
+  }
+
+  let context = canvas.getContext("2d")
+  let width = canvas.width
+  let height = canvas.height
+
+  self.setOpacity = function(a){
+    context.globalAlpha = a
+  }
+
+  self.setFillColor = function(c){
+    fillColor = c
+    return self
+  }
+
+  self.setLineColor = function(c){
+    strokeColor = c
+    return self
+  }
+
+  self.setDotSize = function(s){
+    dotSize = s
+    return self
+  }
+
+  self.setLineThickness = function(t){
+    lineThickness = t
+    return self
+  }
+
+  self.setAxesAreVisible = function(v){
+    axesAreVisible = v
+    return self
+  }
+
+  self.setTextStyle = function(t){
+    textStyle = t
+  }
+
+  self.setRange = function(a, b, c, d){
+    xmin = a
+    xmax = b
+    ymin = c
+    ymax = d
+    return self
+  }
+
+  self.splitTextIntoLines = function(text, maxWidth){
+    let lines = []
+    let words = text.split(" ")
+    let temp = ""
+
+    words.forEach(function(word){
+      let width = context.measureText(temp + " " + word).width
+
+      if (width > maxWidth){
+        lines.push(temp)
+        temp = word
+      } else {
+        temp += " " + word
+      }
+    })
+
+    if (temp.length > 0){
+      lines.push(temp)
+    }
+
+    return lines
+  }
+
+  self.clear = function(){
+    context.clearRect(0, 0, width, height)
+    context.fillStyle = "white"
+    context.fillRect(0, 0, width, height)
+  }
+
+  self.drawAxes = function(){
+    if (axesAreVisible){
+      context.fillStyle = "none"
+      context.strokeStyle = "black"
+      context.lineWidth = 1
+
+      context.beginPath()
+      context.moveTo(-width/2, map(0, ymin, ymax, -height/2, height/2))
+      context.lineTo(width/2, map(0, ymin, ymax, -height/2, height/2))
+      context.stroke()
+
+      context.beginPath()
+      context.moveTo(map(0, xmin, xmax, -width/2, width/2), -height/2)
+      context.lineTo(map(0, xmin, xmax, -width/2, width/2), height/2)
+      context.stroke()
+    }
+
+    return self
+  }
+
+  self.scatter = function(x, y){
+    context.save()
+    context.translate(width/2, height/2)
+    context.scale(1, -1)
+
+    self.drawAxes()
+
+    context.fillStyle = fillColor
+    context.strokeStyle = strokeColor
+    context.lineWidth = lineThickness
+
+    for (let i=0; i<x.length; i++){
+      xTemp = map(x[i], xmin, xmax, -width/2, width/2)
+      yTemp = map(y[i], ymin, ymax, -height/2, height/2)
+
+      context.beginPath()
+      context.ellipse(xTemp, yTemp, dotSize / 2, dotSize / 2, 0, 0, Math.PI * 2)
+      if (fillColor !== "none") context.fill()
+      if (lineThickness > 0) context.stroke()
+    }
+
+    context.restore()
+    return self
+  }
+
+  self.line = function(x, y){
+    context.save()
+    context.translate(width/2, height/2)
+    context.scale(1, -1)
+
+    self.drawAxes()
+
+    context.fillStyle = fillColor
+    context.strokeStyle = strokeColor
+    context.lineWidth = lineThickness
+
+    for (let i=0; i<x.length-1; i++){
+      xTemp1 = map(x[i], xmin, xmax, -width/2, width/2)
+      yTemp1 = map(y[i], ymin, ymax, -height/2, height/2)
+      xTemp2 = map(x[i+1], xmin, xmax, -width/2, width/2)
+      yTemp2 = map(y[i+1], ymin, ymax, -height/2, height/2)
+
+      context.beginPath()
+      context.moveTo(xTemp1, yTemp1)
+      context.lineTo(xTemp2, yTemp2)
+      context.stroke()
+    }
+
+    context.restore()
+    return self
+  }
+
+  self.dottedLine = function(x, y){
+    context.save()
+    context.translate(width/2, height/2)
+    context.scale(1, -1)
+
+    self.drawAxes()
+
+    context.fillStyle = fillColor
+    context.strokeStyle = strokeColor
+    context.lineWidth = lineThickness
+
+    for (let i=0; i<x.length-1; i+=2){
+      try {
+        xTemp1 = map(x[i], xmin, xmax, -width/2, width/2)
+        yTemp1 = map(y[i], ymin, ymax, -height/2, height/2)
+        xTemp2 = map(x[i+1], xmin, xmax, -width/2, width/2)
+        yTemp2 = map(y[i+1], ymin, ymax, -height/2, height/2)
+
+        context.beginPath()
+        context.moveTo(xTemp1, yTemp1)
+        context.lineTo(xTemp2, yTemp2)
+        context.stroke()
+      } catch(e){}
+    }
+
+    context.restore()
+    return self
+  }
+
+  self.bar = function(values, colors){
+    context.save()
+    context.translate(width/2, height/2)
+    context.scale(1, -1)
+
+    context.strokeStyle = strokeColor
+    context.lineWidth = lineThickness
+
+    let barThickness = 0.5
+
+    for (let i=0; i<values.length; i++){
+      context.fillStyle = colors[i]
+
+      let xTemp = map((i + 2) - barThickness / 2, xmin, xmax, -width/2, width/2)
+      let yTemp = map(0, ymin, ymax, -height/2, height/2)
+      let wTemp = map(barThickness, 0, xmax - xmin, 0, width)
+      let hTemp = map(values[i], 0, ymax - ymin, 0, height)
+
+      if (colors[i] !== "none") context.fillRect(xTemp, yTemp, wTemp, hTemp)
+      if (lineThickness > 0) context.strokeRect(xTemp, yTemp, wTemp, hTemp)
+    }
+
+    self.drawAxes()
+    context.restore()
+    return self
+  }
+
+  self.text = function(text, x, y, maxWidth){
+    context.save()
+    context.translate(width/2, height/2)
+    context.scale(1, -1)
+
+    context.fillStyle = textStyle.color
+    context.font = `${textStyle.isBold ? "bold" : ""} ${textStyle.isItalicized ? "italic" : ""} ${textStyle.size}px ${textStyle.family}`
+    context.textAlign = textStyle.alignment
+    context.textBaseline = textStyle.baseline
+
+    let lines
+
+    if (maxWidth){
+      lines = self.splitTextIntoLines(text, map(maxWidth, 0, xmax - xmin, 0, width))
+    } else {
+      lines = [text]
+    }
+
+    lines.forEach(function(line, index){
+      context.save()
+      context.translate(map(x, xmin, xmax, -width/2, width/2), map(y, ymin, ymax, -height/2, height/2) - index * textStyle.lineHeight)
+      context.scale(1, -1)
+      context.fillText(line, 0, 0)
+      context.restore()
+    })
+
+    context.restore()
+    return self
+  }
+
+  self.getContext = function(){
+    return context
+  }
+
+  self.download = function(filename){
+    downloadCanvas(canvas, filename)
+    return self
+  }
+}
