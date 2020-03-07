8caa2dd31bcf1b2b20e6413db88d3601450fcfc7
jrc03c <jrc03c@pm.me>
2020-03-06 19:00:18 -0600

    ...

--- dist/js-math-tools.js
+++ dist/js-math-tools.js
@@ -1639,16 +1639,111 @@ if (!module.parent){
 }
 
 },{"../misc/assert.js":47,"./abs.js":6,"./chop.js":9,"./is-number.js":17,"./is-undefined.js":19,"./vectorize.js":42}],22:[function(require,module,exports){
-let isArray = require("./is-array.js")
+let assert = require("../misc/assert.js")
+let isUndefined = require("./is-undefined.js")
+let isNumber = require("./is-number.js")
+let vectorize = require("./vectorize.js")
 
-function map(x, a, b, c, d){
-  if (isArray(x)) return x.map(v => map(v, a, b, c, d))
+let map = vectorize(function(x, a, b, c, d){
+  assert(!isUndefined(x) && !isUndefined(a) && !isUndefined(b) && !isUndefined(c) && !isUndefined(d), "You should pass five numbers (or five equally-sized arrays of numbers) into the `map` function!")
+
+  assert(isNumber(x) && isNumber(a) && isNumber(b) && isNumber(c) && isNumber(d), "The `map` function only works on numbers!")
+
   return (d - c) * (x - a) / (b - a) + c
-}
+})
 
 module.exports = map
 
-},{"./is-array.js":16}],23:[function(require,module,exports){
+// tests
+if (!module.parent){
+  let x = 1
+  let a = 0
+  let b = 2
+  let c = 0
+  let d = 10
+  let yTrue = 5
+  let yPred = map(x, a, b, c, d)
+  assert(yTrue === yPred, `map(${x}, ${a}, ${b}, ${c}, ${c}) should be ${yTrue}, but instead is ${yPred}!`)
+
+  x = 2
+  a = 1
+  b = 3
+  c = 100
+  d = 500
+  yTrue = 300
+  yPred = map(x, a, b, c, d)
+  assert(yTrue === yPred, `map(${x}, ${a}, ${b}, ${c}, ${c}) should be ${yTrue}, but instead is ${yPred}!`)
+
+  x = [1, 2, 3]
+  a = 0
+  b = 4
+  c = 100
+  d = 500
+  yTrue = [200, 300, 400]
+  yPred = map(x, a, b, c, d)
+  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `map(${x[i]}, ${a}, ${b}, ${c}, ${d}) should be ${yTrue[i]}, but instead was ${yPred[i]}!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, "five")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, "five") should have failed!`)
+
+  try {
+    hasFailed = false
+    map()
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map() should have failed!`)
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, {}) should have failed!`)
+
+  try {
+    let foo
+    hasFailed = false
+    map(1, 2, 3, 4, foo)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, foo) should have failed!`)
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, () => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, () => {}) should have failed!`)
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, true)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, true) should have failed!`)
+
+  console.log("All tests passed!")
+}
+
+},{"../misc/assert.js":47,"./is-number.js":17,"./is-undefined.js":19,"./vectorize.js":42}],23:[function(require,module,exports){
 function max(arr){
   let out
 
--- src/math/map.js
+++ src/math/map.js
@@ -1,8 +1,103 @@
-let isArray = require("./is-array.js")
+let assert = require("../misc/assert.js")
+let isUndefined = require("./is-undefined.js")
+let isNumber = require("./is-number.js")
+let vectorize = require("./vectorize.js")
 
-function map(x, a, b, c, d){
-  if (isArray(x)) return x.map(v => map(v, a, b, c, d))
+let map = vectorize(function(x, a, b, c, d){
+  assert(!isUndefined(x) && !isUndefined(a) && !isUndefined(b) && !isUndefined(c) && !isUndefined(d), "You should pass five numbers (or five equally-sized arrays of numbers) into the `map` function!")
+
+  assert(isNumber(x) && isNumber(a) && isNumber(b) && isNumber(c) && isNumber(d), "The `map` function only works on numbers!")
+
   return (d - c) * (x - a) / (b - a) + c
-}
+})
 
 module.exports = map
+
+// tests
+if (!module.parent){
+  let x = 1
+  let a = 0
+  let b = 2
+  let c = 0
+  let d = 10
+  let yTrue = 5
+  let yPred = map(x, a, b, c, d)
+  assert(yTrue === yPred, `map(${x}, ${a}, ${b}, ${c}, ${c}) should be ${yTrue}, but instead is ${yPred}!`)
+
+  x = 2
+  a = 1
+  b = 3
+  c = 100
+  d = 500
+  yTrue = 300
+  yPred = map(x, a, b, c, d)
+  assert(yTrue === yPred, `map(${x}, ${a}, ${b}, ${c}, ${c}) should be ${yTrue}, but instead is ${yPred}!`)
+
+  x = [1, 2, 3]
+  a = 0
+  b = 4
+  c = 100
+  d = 500
+  yTrue = [200, 300, 400]
+  yPred = map(x, a, b, c, d)
+  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `map(${x[i]}, ${a}, ${b}, ${c}, ${d}) should be ${yTrue[i]}, but instead was ${yPred[i]}!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, "five")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, "five") should have failed!`)
+
+  try {
+    hasFailed = false
+    map()
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map() should have failed!`)
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, {}) should have failed!`)
+
+  try {
+    let foo
+    hasFailed = false
+    map(1, 2, 3, 4, foo)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, foo) should have failed!`)
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, () => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, () => {}) should have failed!`)
+
+  try {
+    hasFailed = false
+    map(1, 2, 3, 4, true)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `map(1, 2, 3, 4, true) should have failed!`)
+
+  console.log("All tests passed!")
+}
