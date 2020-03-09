7c1b9c70a1741b781f65b99eadc5b94f9a0fc903
jrc03c <jrc03c@pm.me>
2020-03-09 13:47:21 -0500

    ...

--- dist/js-math-tools.js
+++ dist/js-math-tools.js
@@ -3097,15 +3097,81 @@ if (!module.parent){
 },{"../misc/assert.js":56,"./is-number.js":20,"./is-undefined.js":22,"./vectorize.js":51}],36:[function(require,module,exports){
 let ndarray = require("./ndarray.js")
 let apply = require("../misc/apply.js")
+let isUndefined = require("./is-undefined.js")
 
 function random(shape){
-  if (!shape) return Math.random()
+  if (isUndefined(shape)) return Math.random()
   return apply(ndarray(shape), Math.random)
 }
 
 module.exports = random
 
-},{"../misc/apply.js":54,"./ndarray.js":31}],37:[function(require,module,exports){
+// tests
+if (!module.parent){
+  let assert = require("../misc/assert.js")
+  let min = require("./min.js")
+  let max = require("./max.js")
+  let abs = require("./abs.js")
+  let mean = require("./mean.js")
+
+  let x = random([10, 10, 10, 10])
+  assert(min(x) >= 0 && max(x) <= 1, `random([10, 10, 10, 10]) should be in the range [0, 1]!`)
+  assert(abs(mean(x)) - 0.5 < 0.05, `random([10, 10, 10, 10]) should have a mean of approximately 0.5!`)
+
+  x = random()
+  assert(x >= 0 && x <= 1, `random() should be in the range [0, 1]!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    random("foo")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random("foo") should have failed!`)
+
+  try {
+    hasFailed = false
+    random(true)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random(true) should have failed!`)
+
+  try {
+    hasFailed = false
+    random({})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random({}) should have failed!`)
+
+  try {
+    hasFailed = false
+    random(() => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random(() => {}) should have failed!`)
+
+  try {
+    hasFailed = false
+    random([1, 2, "three"])
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random([1, 2, "three"]) should have failed!`)
+
+  console.log("All tests passed!")
+}
+
+},{"../misc/apply.js":54,"../misc/assert.js":56,"./abs.js":6,"./is-undefined.js":22,"./max.js":26,"./mean.js":27,"./min.js":29,"./ndarray.js":31}],37:[function(require,module,exports){
 function range(a, b, step=1){
   let out = []
   for (let i=a; i<b; i+=step) out.push(i)
--- src/math/random.js
+++ src/math/random.js
@@ -1,9 +1,75 @@
 let ndarray = require("./ndarray.js")
 let apply = require("../misc/apply.js")
+let isUndefined = require("./is-undefined.js")
 
 function random(shape){
-  if (!shape) return Math.random()
+  if (isUndefined(shape)) return Math.random()
   return apply(ndarray(shape), Math.random)
 }
 
 module.exports = random
+
+// tests
+if (!module.parent){
+  let assert = require("../misc/assert.js")
+  let min = require("./min.js")
+  let max = require("./max.js")
+  let abs = require("./abs.js")
+  let mean = require("./mean.js")
+
+  let x = random([10, 10, 10, 10])
+  assert(min(x) >= 0 && max(x) <= 1, `random([10, 10, 10, 10]) should be in the range [0, 1]!`)
+  assert(abs(mean(x)) - 0.5 < 0.05, `random([10, 10, 10, 10]) should have a mean of approximately 0.5!`)
+
+  x = random()
+  assert(x >= 0 && x <= 1, `random() should be in the range [0, 1]!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    random("foo")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random("foo") should have failed!`)
+
+  try {
+    hasFailed = false
+    random(true)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random(true) should have failed!`)
+
+  try {
+    hasFailed = false
+    random({})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random({}) should have failed!`)
+
+  try {
+    hasFailed = false
+    random(() => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random(() => {}) should have failed!`)
+
+  try {
+    hasFailed = false
+    random([1, 2, "three"])
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `random([1, 2, "three"]) should have failed!`)
+
+  console.log("All tests passed!")
+}
