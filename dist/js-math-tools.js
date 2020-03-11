5b96d51d7ac1ccba663735e515f8cf66002892ba
jrc03c <jrc03c@pm.me>
2020-03-11 17:54:17 -0500

    ...

--- dist/js-math-tools.js
+++ dist/js-math-tools.js
@@ -3946,11 +3946,90 @@ if (!module.parent){
 }
 
 },{"../misc/assert.js":56,"./add.js":7,"./chop.js":9,"./is-number.js":20,"./is-undefined.js":22,"./normal.js":32,"./random.js":36,"./round.js":38,"./scale.js":39,"./set.js":40,"./sort.js":45,"./vectorize.js":51}],44:[function(require,module,exports){
+let assert = require("../misc/assert.js")
+let isUndefined = require("./is-undefined.js")
+let isNumber = require("./is-number.js")
 let vectorize = require("./vectorize.js")
-let sin = vectorize(Math.sin)
+
+let sin = vectorize(function(x){
+  assert(!isUndefined(x), "You must pass a number or an array of numbers into the `sin` function!")
+  assert(isNumber(x), "You must pass a number or an array of numbers into the `sin` function!")
+
+  return Math.sin(x)
+})
+
 module.exports = sin
 
-},{"./vectorize.js":51}],45:[function(require,module,exports){
+// tests
+if (!module.parent){
+  let min = require("./min.js")
+  let max = require("./max.js")
+  let range = require("./range.js")
+
+  let x = sin(range(0, 10 * Math.PI, Math.PI / 180))
+  assert(min(x) === -1 && max(x) === 1, `sin(range(0, 10 * Math.PI, Math.PI / 100)) should be in the range [-1, 1]!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    sin()
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin() should have failed!`)
+
+  try {
+    hasFailed = false
+    sin("foo")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin("foo") should have failed!`)
+
+  try {
+    hasFailed = false
+    sin(true)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin(true) should have failed!`)
+
+  try {
+    hasFailed = false
+    sin({})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin({}) should have failed!`)
+
+  try {
+    hasFailed = false
+    sin(() => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin(() => {}) should have failed!`)
+
+  try {
+    let foo
+    hasFailed = false
+    sin(foo)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin(foo) should have failed!`)
+
+  console.log("All tests passed!")
+}
+
+},{"../misc/assert.js":56,"./is-number.js":20,"./is-undefined.js":22,"./max.js":26,"./min.js":29,"./range.js":37,"./vectorize.js":51}],45:[function(require,module,exports){
 function sort(arr, fn){
   let out = arr.slice()
   out.sort(fn)
--- src/math/sin.js
+++ src/math/sin.js
@@ -1,3 +1,82 @@
+let assert = require("../misc/assert.js")
+let isUndefined = require("./is-undefined.js")
+let isNumber = require("./is-number.js")
 let vectorize = require("./vectorize.js")
-let sin = vectorize(Math.sin)
+
+let sin = vectorize(function(x){
+  assert(!isUndefined(x), "You must pass a number or an array of numbers into the `sin` function!")
+  assert(isNumber(x), "You must pass a number or an array of numbers into the `sin` function!")
+
+  return Math.sin(x)
+})
+
 module.exports = sin
+
+// tests
+if (!module.parent){
+  let min = require("./min.js")
+  let max = require("./max.js")
+  let range = require("./range.js")
+
+  let x = sin(range(0, 10 * Math.PI, Math.PI / 180))
+  assert(min(x) === -1 && max(x) === 1, `sin(range(0, 10 * Math.PI, Math.PI / 100)) should be in the range [-1, 1]!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    sin()
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin() should have failed!`)
+
+  try {
+    hasFailed = false
+    sin("foo")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin("foo") should have failed!`)
+
+  try {
+    hasFailed = false
+    sin(true)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin(true) should have failed!`)
+
+  try {
+    hasFailed = false
+    sin({})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin({}) should have failed!`)
+
+  try {
+    hasFailed = false
+    sin(() => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin(() => {}) should have failed!`)
+
+  try {
+    let foo
+    hasFailed = false
+    sin(foo)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `sin(foo) should have failed!`)
+
+  console.log("All tests passed!")
+}
