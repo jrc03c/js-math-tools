50e8efd979bdbd32f87453322adb085faf553f90
jrc03c <jrc03c@pm.me>
2020-03-16 19:41:34 -0500

    ...

--- dist/js-math-tools.js
+++ dist/js-math-tools.js
@@ -4604,11 +4604,113 @@ if (!module.parent){
 }
 
 },{"../misc/assert.js":57,"./abs.js":6,"./flatten.js":17,"./is-array.js":19,"./is-number.js":21,"./is-undefined.js":23,"./normal.js":33,"./range.js":38}],50:[function(require,module,exports){
+let assert = require("../misc/assert.js")
+let isUndefined = require("./is-undefined.js")
+let isNumber = require("./is-number.js")
 let vectorize = require("./vectorize.js")
-let tan = vectorize(Math.tan)
+let floor = require("./floor.js")
+
+let tan = vectorize(function(x){
+  assert(!isUndefined(x), "You must pass a number or an array of numbers into the `tan` function!")
+  assert(isNumber(x), "You must pass a number or an array of numbers into the `tan` function!")
+
+  let k = (x - Math.PI / 2) / Math.PI
+  if (k === floor(k)) return undefined
+  return Math.tan(x)
+})
+
 module.exports = tan
 
-},{"./vectorize.js":52}],51:[function(require,module,exports){
+// tests
+if (!module.parent){
+  let abs = require("./abs.js")
+  let normal = require("./normal.js")
+
+  let x = Math.PI / 4
+  let yTrue = 1
+  let yPred = tan(x)
+  assert(abs(yTrue - yPred) < 0.01, `tan(pi / 4) should be 1, but instead was ${yPred}!`)
+
+  x = -Math.PI / 2
+  yTrue = undefined
+  yPred = tan(x)
+  assert(yTrue === yPred, "tan(-pi / 2) should be undefined, but instead was ${yPred}!")
+
+  x = 2 * Math.PI
+  yTrue = 0
+  yPred = tan(x)
+  assert(abs(yTrue - yPred) < 0.01, `tan(2 * pi) should be 0, but instead was ${yPred}!`)
+
+  let hasFailed
+
+  try {
+    hasFailed = false
+    tan()
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `tan() should have failed!`)
+
+  try {
+    hasFailed = false
+    tan(normal(10000))
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(!hasFailed, `tan(normal(10000)) should not have failed!`)
+
+  try {
+    hasFailed = false
+    tan("foo")
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `tan("foo") should have failed!`)
+
+  try {
+    hasFailed = false
+    tan(true,)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `tan(true) should have failed!`)
+
+  try {
+    hasFailed = false
+    tan({})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `tan({}) should have failed!`)
+
+  try {
+    hasFailed = false
+    tan(() => {})
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `tan(() => {}) should have failed!`)
+
+  try {
+    let foo
+    hasFailed = false
+    tan(foo)
+  } catch(e){
+    hasFailed = true
+  }
+
+  assert(hasFailed, `tan(foo) should have failed!`)
+
+  console.log("All tests passed!")
+}
+
+},{"../misc/assert.js":57,"./abs.js":6,"./floor.js":18,"./is-number.js":21,"./is-undefined.js":23,"./normal.js":33,"./vectorize.js":52}],51:[function(require,module,exports){
 let pow = require("./pow.js")
 let std = require("./std.js")
 
