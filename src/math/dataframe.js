0ae7a798c8266a3282b563d96e46930b4b5847d2
jrc03c <jrc03c@pm.me>
2021-02-14 12:45:12 -0600

    ...

--- src/math/dataframe.js
+++ src/math/dataframe.js
@@ -236,6 +236,84 @@ class DataFrame {
 
     return out
   }
+
+  dropMissing(axis, condition, threshold){
+    axis = axis || 0
+    assert(axis === 0 || axis === 1, "The first parameter of the `dropMissing` method (the `axis`) must be 0 or 1.")
+
+    threshold = threshold || 0
+    assert(isWholeNumber(threshold), "The third parameter of the `dropMissing` method (the `threshold`) should be a whole number (meaning that data should be dropped if it contains more than `threshold` null values).")
+
+    condition = threshold > 0 ? "none" : (condition || "any")
+    assert(condition === "any" || condition === "all" || condition === "none", "The second parameter of the `dropMissing` method (the `condition` parameter, which indicates the condition under which data should be dropped) should be 'any' or 'all' (meaning that if 'any' of the data contains null values, then it should be dropped; or that if 'all' of the data contains null values, then it should be dropped).")
+
+    function helper(values){
+      if (threshold > 0){
+        let count = 0
+
+        for (let i=0; i<values.length; i++){
+          let value = values[i]
+          if (isUndefined(value)) count++
+          if (count >= threshold) return []
+        }
+      } else if (condition === "any"){
+        for (let i=0; i<values.length; i++){
+          let value = values[i]
+          if (isUndefined(value)) return []
+        }
+      } else if (condition === "all"){
+        for (let i=0; i<values.length; i++){
+          let value = values[i]
+          if (!isUndefined(value)) return values
+        }
+
+        return []
+      }
+
+      return values
+    }
+
+    let self = this
+    let out = self.copy()
+
+    if (axis === 0){
+      let newIndex = copy(out.index)
+
+      let newValues = out.values.map(helper).filter((row, i) => {
+        if (row.length === 0){
+          newIndex.splice(i, 1)
+          return false
+        } else {
+          return true
+        }
+      })
+
+      if (shape(newValues).length < 2) return new DataFrame()
+
+      out.values = newValues
+      out.index = newIndex
+    } else if (axis === 1){
+      out = out.transpose()
+      let newIndex = copy(out.index)
+
+      let newValues = out.values.map(helper).filter((col, i) => {
+        if (col.length === 0){
+          newIndex.splice(i, 1)
+          return false
+        } else {
+          return true
+        }
+      })
+
+      if (shape(newValues).length < 2) return new DataFrame()
+
+      out.values = newValues
+      out.index = newIndex
+      out = out.transpose()
+    }
+
+    return out
+  }
 }
 
 module.exports = DataFrame
