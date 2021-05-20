1187fe6773ae28fd51984e12d90530ff7b74ff52
jrc03c <jrc03c@pm.me>
2021-05-20 09:02:40 -0500

    ...

--- src/abs.test.js
+++ src/abs.test.js
@@ -23,3 +23,23 @@ test("takes absolute value of -3.5 to be 3.5", () => {
 test("takes absolute value of [2, -3, 4, -5] to be [2, 3, 4, 5]", () => {
   expect(abs([2, -3, 4, -5])).toStrictEqual([2, 3, 4, 5])
 })
+
+test("takes absolute value of 'foo' to be NaN", () => {
+  expect(abs("foo")).toBeNaN()
+})
+
+test("takes absolute value of `true` to be NaN", () => {
+  expect(abs(true)).toBe(1)
+})
+
+test("takes absolute value of `false` to be NaN", () => {
+  expect(abs(false)).toBe(0)
+})
+
+test("takes absolute value of `() => {}` to be NaN", () => {
+  expect(abs(() => {})).toBeNaN()
+})
+
+test("takes absolute value of `{}` to be NaN", () => {
+  expect(abs({})).toBeNaN()
+})
