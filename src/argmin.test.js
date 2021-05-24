4c66a8b2f4894bb6418647f1270ef0f09c36e543
jrc03c <jrc03c@pm.me>
2021-05-23 21:51:35 -0500

    ...

---
+++ src/argmin.test.js
@@ -0,0 +1,22 @@
+const argmin = require("./argmin.js")
+const shuffle = require("./shuffle.js")
+const range = require("./range.js")
+
+test("gets the argmin of a linear range of values", () => {
+  const x = shuffle(range(0, 100))
+  const indexTrue = x.indexOf(0)
+  const indexPred = argmin(x)[0]
+  expect(indexPred).toStrictEqual(indexTrue)
+})
+
+test("gets the argmin of a predefined array of values", () => {
+  const x = [
+    [2, 3, 4],
+    [0, 1, 2],
+    [-5, 10, 20],
+  ]
+
+  const indexTrue = [2, 0]
+  const indexPred = argmin(x)
+  expect(indexPred).toStrictEqual(indexTrue)
+})
