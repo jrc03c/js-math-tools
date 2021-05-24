833077d18f5442b19f6f9588074304783ef55d6b
jrc03c <jrc03c@pm.me>
2021-05-23 21:07:46 -0500

    ...

--- src/add.js
+++ src/add.js
@@ -1,26 +1,26 @@
-let assert = require("./assert.js")
-let vectorize = require("./vectorize.js")
-let isNumber = require("./is-number.js")
-let isString = require("./is-string.js")
-let isUndefined = require("./is-undefined.js")
+const assert = require("./assert.js")
+const vectorize = require("./vectorize.js")
+const isNumber = require("./is-number.js")
+const isString = require("./is-string.js")
+const isUndefined = require("./is-undefined.js")
 
-let add = vectorize(function(){
+const add = vectorize(function () {
   try {
-		let out
-    let keys = Object.keys(arguments)
+    let out
+    const keys = Object.keys(arguments)
 
-    for (let i=0; i<keys.length; i++){
-      let key = keys[i]
-      let value = arguments[key]
-      if (typeof(value) !== "number") return NaN
-			if (!out) out = value
-			else out += arguments[key]
-		}
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i]
+      const value = arguments[key]
+      if (typeof value !== "number") return NaN
+      if (!out) out = value
+      else out += arguments[key]
+    }
 
-		return out
-	} catch(e) {
-		return NaN
-	}
+    return out
+  } catch (e) {
+    return NaN
+  }
 })
 
 module.exports = add
