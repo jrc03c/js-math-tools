01e9c338e4987d940a50119214c7fc2cc263aa17
jrc03c <jrc03c@pm.me>
2021-05-25 18:18:34 -0500

    ...

--- src/count.js
+++ src/count.js
@@ -1,18 +1,29 @@
-let assert = require("./assert.js")
-let isUndefined = require("./is-undefined.js")
-let isArray = require("./is-array.js")
-let flatten = require("./flatten.js")
+const assert = require("./assert.js")
+const isUndefined = require("./is-undefined.js")
+const isArray = require("./is-array.js")
+const flatten = require("./flatten.js")
+const isEqual = require("./is-equal.js")
+const set = require("./set.js")
 
-function count(arr, items){
-  assert(!isUndefined(arr), "You must an array and an item to count to the `count` function!")
-  assert(isArray(arr), "You must an array and an item to count to the `count` function!")
+function count(arr, items) {
+  assert(
+    !isUndefined(arr),
+    "You must pass an array and some items to count into the `count` function!"
+  )
+
+  assert(
+    isArray(arr),
+    "You must pass an array and some items to count into the `count` function!"
+  )
 
   // NOTE: This currently flattens the array that's passed in, which means that it's not possible to count occurrences of arrays within arrays! I'm not sure whether this is desirable behavior or not, so I'm just making a note of it for now. It's not trivial to count occurrences of identical objects, so maybe this function should refuse to operate on objects!
-  let temp = flatten(arr)
+  const temp = flatten(arr)
+  items = isUndefined(items) ? set(arr) : items
 
-  if (isArray(items)){
-    return flatten(items).map(function(item1){
-      return temp.filter(item2 => item2 === item1).length
+  if (isArray(items)) {
+    return flatten(items).map(function (item1) {
+      const c = temp.filter(item2 => isEqual(item1, item2)).length
+      return { item: item1, count: c }
     })
   } else {
     return temp.filter(other => other === items).length
@@ -20,86 +31,3 @@ function count(arr, items){
 }
 
 module.exports = count
-
-// tests
-if (!module.parent && typeof(window) === "undefined"){
-  let random = require("./random.js")
-  let round = require("./round.js")
-  let abs = require("./abs.js")
-
-  let x = [2, 2, 2, 3, 4, 2, 2]
-  let yTrue = 5
-  let yPred = count(x, 2)
-  assert(yTrue === yPred)
-
-  x = [true, true, false, false, false, "a", "a", "a", "a", "a"]
-  yTrue = [2, 3, 5]
-  yPred = count(x, [true, false, "a"])
-  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `count([true, true, false, false, false, "a", "a", "a", "a", "a"], [true, false, "a"]) should be [2, 3, 5]!`)
-
-  x = round(random([10000]))
-  let y1 = count(x, 0)
-  let y2 = count(x, 1)
-  assert(abs(y1 - 5000) < 0.05 * 5000, `count(round(random([10000])), 0) should be approximately 5000!`)
-  assert(abs(y2 - 5000) < 0.05 * 5000, `count(round(random([10000])), 1) should be approximately 5000!`)
-
-  assert(count([2, 3, 4]) === 0, `count([2, 3, 4]) should be 0!`)
-
-  let hasFailed
-
-  try {
-    hasFailed = false
-    count()
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `count() should have failed!`)
-
-  try {
-    hasFailed = false
-    count(234)
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `count(234) should have failed!`)
-
-  try {
-    hasFailed = false
-    count(true)
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `count(true) should have failed!`)
-
-  try {
-    hasFailed = false
-    count("foo")
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `count("foo") should have failed!`)
-
-  try {
-    hasFailed = false
-    count({})
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `count({}) should have failed!`)
-
-  try {
-    hasFailed = false
-    count(() => {})
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `count(() => {}) should have failed!`)
-
-  console.log("All tests passed!")
-}
--- src/set.js
+++ src/set.js
@@ -1,16 +1,25 @@
-let assert = require("./assert.js")
-let isUndefined = require("./is-undefined.js")
-let isArray = require("./is-array.js")
-let flatten = require("./flatten.js")
+const assert = require("./assert.js")
+const isUndefined = require("./is-undefined.js")
+const isArray = require("./is-array.js")
+const flatten = require("./flatten.js")
 
-function set(arr){
+function set(arr) {
   assert(!isUndefined(arr), "You must pass an array into the `set` function!")
   assert(isArray(arr), "You must pass an array into the `set` function!")
 
-  let out = []
+  const out = []
+  const temp = {}
 
-  flatten(arr).forEach(function(item){
-    if (out.indexOf(item) < 0) out.push(item)
+  flatten(arr).forEach(function (item) {
+    const key =
+      typeof item === "undefined"
+        ? "undefined"
+        : typeof item === "function"
+        ? item.toString()
+        : JSON.stringify(item)
+
+    if (!temp[key]) out.push(item)
+    temp[key] = true
   })
 
   return out
@@ -18,104 +27,120 @@ function set(arr){
 
 module.exports = set
 
-// tests
-if (!module.parent && typeof(window) === "undefined"){
-  let sort = require("./sort.js")
-  let round = require("./round.js")
-  let random = require("./random.js")
-  let range = require("./range.js")
-
-  function alphasort(a, b){
-    if (a < b) return -1
-    if (a > b) return 1
-    return 0
-  }
-
-  let x = [2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 3, 4, 3, 2, 2, 3, 3, 3, 3, 4]
-  let yTrue = [2, 3, 4]
-  let yPred = sort(set(x), alphasort)
-  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `set([2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 3, 4, 3, 2, 2, 3, 3, 3, 3, 4]) should be [2, 3, 4]!`)
-
-  x = round(random([10, 10, 10, 10]))
-  yTrue = [0, 1]
-  yPred = sort(set(x), alphasort)
-  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `set(round(random([10, 10, 10, 10]))) should be [0, 1]!`)
-
-  x = range(10, 20, 0.25)
-  yTrue = x.slice()
-  yPred = set(x)
-  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `set(range(10, 20, 0.25)) should be the same as range(10, 20, 0.25)!`)
-
-  x = ["foo", "bar", "baz", "foo", "foo", true, true, false, true, 234, 234, 0]
-  yTrue = ["foo", "bar", "baz", true, false, 234, 0]
-  yPred = set(x)
-  for (let i=0; i<yTrue.length; i++) assert(yTrue[i] === yPred[i], `set(["foo", "bar", "baz", "foo", "foo", true, true, false, true, 234, 234, 0]) should be ["foo", "bar", "baz", true, false, 234, 0]!`)
-
-  let hasFailed
-
-  try {
-    hasFailed = false
-    set()
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set() should have failed!`)
-
-  try {
-    hasFailed = false
-    set("foo")
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set("foo") should have failed!`)
-
-  try {
-    hasFailed = false
-    set(234)
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set(234) should have failed!`)
-
-  try {
-    hasFailed = false
-    set(true)
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set(true) should have failed!`)
-
-  try {
-    hasFailed = false
-    set({})
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set({}) should have failed!`)
-
-  try {
-    hasFailed = false
-    set(() => {})
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set(() => {}) should have failed!`)
-
-  try {
-    let foo
-    hasFailed = false
-    set(foo)
-  } catch(e){
-    hasFailed = true
-  }
-
-  assert(hasFailed, `set(foo) should have failed!`)
-
-  console.log("All tests passed!")
-}
+// // tests
+// if (!module.parent && typeof window === "undefined") {
+//   const sort = require("./sort.js")
+//   const round = require("./round.js")
+//   const random = require("./random.js")
+//   const range = require("./range.js")
+//
+//   function alphasort(a, b) {
+//     if (a < b) return -1
+//     if (a > b) return 1
+//     return 0
+//   }
+//
+//   const x = [2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 3, 4, 3, 2, 2, 3, 3, 3, 3, 4]
+//   const yTrue = [2, 3, 4]
+//   const yPred = sort(set(x), alphasort)
+//   for (const i = 0; i < yTrue.length; i++)
+//     assert(
+//       yTrue[i] === yPred[i],
+//       `set([2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 3, 4, 3, 2, 2, 3, 3, 3, 3, 4]) should be [2, 3, 4]!`
+//     )
+//
+//   x = round(random([10, 10, 10, 10]))
+//   yTrue = [0, 1]
+//   yPred = sort(set(x), alphasort)
+//   for (const i = 0; i < yTrue.length; i++)
+//     assert(
+//       yTrue[i] === yPred[i],
+//       `set(round(random([10, 10, 10, 10]))) should be [0, 1]!`
+//     )
+//
+//   x = range(10, 20, 0.25)
+//   yTrue = x.slice()
+//   yPred = set(x)
+//   for (const i = 0; i < yTrue.length; i++)
+//     assert(
+//       yTrue[i] === yPred[i],
+//       `set(range(10, 20, 0.25)) should be the same as range(10, 20, 0.25)!`
+//     )
+//
+//   x = ["foo", "bar", "baz", "foo", "foo", true, true, false, true, 234, 234, 0]
+//   yTrue = ["foo", "bar", "baz", true, false, 234, 0]
+//   yPred = set(x)
+//   for (const i = 0; i < yTrue.length; i++)
+//     assert(
+//       yTrue[i] === yPred[i],
+//       `set(["foo", "bar", "baz", "foo", "foo", true, true, false, true, 234, 234, 0]) should be ["foo", "bar", "baz", true, false, 234, 0]!`
+//     )
+//
+//   const hasFailed
+//
+//   try {
+//     hasFailed = false
+//     set()
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set() should have failed!`)
+//
+//   try {
+//     hasFailed = false
+//     set("foo")
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set("foo") should have failed!`)
+//
+//   try {
+//     hasFailed = false
+//     set(234)
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set(234) should have failed!`)
+//
+//   try {
+//     hasFailed = false
+//     set(true)
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set(true) should have failed!`)
+//
+//   try {
+//     hasFailed = false
+//     set({})
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set({}) should have failed!`)
+//
+//   try {
+//     hasFailed = false
+//     set(() => {})
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set(() => {}) should have failed!`)
+//
+//   try {
+//     const foo
+//     hasFailed = false
+//     set(foo)
+//   } catch (e) {
+//     hasFailed = true
+//   }
+//
+//   assert(hasFailed, `set(foo) should have failed!`)
+//
+//   console.log("All tests passed!")
+// }
