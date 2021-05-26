e981c04dd9993d48309d63a77f053e43621f8b33
jrc03c <jrc03c@pm.me>
2021-05-26 17:37:52 -0500

    ...

---
+++ src/ones.test.js
@@ -0,0 +1,43 @@
+const ones = require("./ones.js")
+const shape = require("./shape.js")
+const set = require("./set.js")
+
+test("gets a tensor of ones", () => {
+  const x = ones([2, 3, 4, 5])
+  expect(set(x)).toStrictEqual([1])
+  expect(shape(x)).toStrictEqual([2, 3, 4, 5])
+})
+
+test("throws an error when attempting to get ones with non-whole-number arguments", () => {
+  expect(() => {
+    ones(-1)
+  }).toThrow()
+
+  expect(() => {
+    ones([-2, -3, -4])
+  }).toThrow()
+
+  expect(() => {
+    ones({})
+  }).toThrow()
+
+  expect(() => {
+    ones(true)
+  }).toThrow()
+
+  expect(() => {
+    ones(false)
+  }).toThrow()
+
+  expect(() => {
+    ones(null)
+  }).toThrow()
+
+  expect(() => {
+    ones(undefined)
+  }).toThrow()
+
+  expect(() => {
+    ones(() => {})
+  }).toThrow()
+})
