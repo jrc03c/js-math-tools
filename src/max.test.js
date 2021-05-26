dc3cca3eecfc54c2b94c6a0ced94ed51b5cf2a91
jrc03c <jrc03c@pm.me>
2021-05-26 13:46:41 -0500

    ...

---
+++ src/max.test.js
@@ -0,0 +1,24 @@
+const max = require("./max.js")
+const normal = require("./normal.js")
+const random = require("./random.js")
+const min = require("./min.js")
+
+test("gets the max of arrays of values of mixed types", () => {
+  expect(max([2, 3, 4])).toBe(4)
+  expect(max([-10, -5, -20])).toBe(-5)
+  expect(max(random([10, 10, 10, 10]))).toBeLessThanOrEqual(1)
+  expect(max(random([10, 10, 10, 10]))).toBeGreaterThanOrEqual(0)
+  expect(max([2, 3, "four"])).toBe(3)
+  expect(max([null, undefined, 3])).toBe(3)
+  expect(max([true, false])).toBe(true)
+})
+
+test("returns NaN when attempting to get the max of non-arrays", () => {
+  expect(max()).toBeNaN()
+  expect(max("foo")).toBeNaN()
+  expect(max(null)).toBeNaN()
+  expect(max(undefined)).toBeNaN()
+  expect(max(() => {})).toBeNaN()
+  expect(max({})).toBeNaN()
+  expect(max([])).toBeNaN()
+})
