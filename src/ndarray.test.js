f3dac0b9d576a5e4f9eea7255d83ce0d50c73045
jrc03c <jrc03c@pm.me>
2021-05-26 17:27:28 -0500

    ...

--- src/ndarray.test.js
+++ src/ndarray.test.js
@@ -1,6 +1,7 @@
 const ndarray = require("./ndarray.js")
 const flatten = require("./flatten.js")
 const dropMissing = require("./drop-missing.js")
+const shape = require("./shape.js")
 
 test("creates ndarrays of various shapes", () => {
   expect(ndarray(3).length).toBe(3)
@@ -8,6 +9,7 @@ test("creates ndarrays of various shapes", () => {
   expect(ndarray([3, 2]).length).toBe(3)
   expect(flatten(ndarray([2, 3, 4])).length).toBe(24)
   expect(dropMissing(flatten(ndarray([2, 3, 4]))).length).toBe(0)
+  expect(shape(ndarray([2, 3, 4]))).toStrictEqual([2, 3, 4])
 })
 
 test("throws an error when attempting to create an ndarray with non-whole-number values", () => {
