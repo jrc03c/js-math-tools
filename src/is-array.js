95a38e1bf79b01c1ba04a3ddce170732ee963f90
jrc03c <jrc03c@pm.me>
2021-05-26 12:08:26 -0500

    ...

--- src/is-array.js
+++ src/is-array.js
@@ -1,23 +1,5 @@
-function isArray(obj){
+function isArray(obj) {
   return obj instanceof Array
 }
 
 module.exports = isArray
-
-// tests
-if (!module.parent && typeof(window) === "undefined"){
-  let assert = require("./assert.js")
-
-  assert(isArray([]), `isArray([]) should return true!`)
-  assert(isArray([2, 3, 4]), `isArray([2, 3, 4]) should return true!`)
-  assert(isArray(new Array()), `isArray(new Array()) should return true!`)
-  assert(!isArray({}), `isArray({}) should return false!`)
-  assert(!isArray({push: () => {}}), `isArray({push: () => {}}) should return false!`)
-  assert(!isArray("foo"), `isArray("foo") should return false!`)
-  assert(!isArray(true), `isArray(true) should return false!`)
-  assert(!isArray(false), `isArray(false) should return false!`)
-  assert(!isArray(() => {}), `isArray(() => {}) should return false!`)
-  assert(!isArray(3), `isArray(3) should return false!`)
-
-  console.log("All tests passed!")
-}
