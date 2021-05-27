2b4e77c9d03ba10b74f8f8a7fec3d5f770e1dff9
jrc03c <jrc03c@pm.me>
2021-05-26 22:16:41 -0500

    ...

---
+++ src/series.test.js
@@ -0,0 +1,61 @@
+const Series = require("./series.js")
+const isEqual = require("./is-equal.js")
+const normal = require("./normal.js")
+const set = require("./set.js")
+const distance = require("./distance.js")
+const zeros = require("./zeros.js")
+const chop = require("./chop.js")
+const random = require("./random.js")
+const isUndefined = require("./is-undefined.js")
+const scale = require("./scale.js")
+const range = require("./range.js")
+const isSeries = s => s instanceof Series
+
+test("tests Series stuff", () => {
+  const x = normal(100)
+  const series = new Series(x)
+  const seriesShape = series.shape
+
+  expect(isSeries(series)).toBe(true)
+  expect(series.shape).toStrictEqual([100])
+  expect(series.isEmpty()).toBe(false)
+  expect(new Series().isEmpty()).toBe(true)
+  expect(series.apply(v => v * 2).values).toStrictEqual(scale(x, 2))
+
+  const clearedValues = set(series.clear().values)
+  expect(clearedValues.length).toBe(1)
+  expect(isUndefined(clearedValues[0])).toBe(true)
+
+  expect(series.values).toStrictEqual(series.reverse().reverse().values)
+  expect(series.get(range(10, 15)).values).toStrictEqual(x.slice(10, 15))
+  expect(series.iloc(range(10, 15)).values).toStrictEqual(x.slice(10, 15))
+  expect(series.loc(series.index.slice(10, 15)).values).toStrictEqual(
+    x.slice(10, 15)
+  )
+
+  const series2 = series.copy()
+  expect(isEqual(series, series2)).toBe(true)
+  expect(series === series2).toBe(false)
+
+  const series3 = new Series([2, 3, null, 5])
+  expect(series3.dropMissing().values).toStrictEqual([2, 3, 5])
+
+  const series4 = new Series([2, 5, 3, 4])
+  expect(series4.sort().values).toStrictEqual([2, 3, 4, 5])
+  expect(series4.sort(true).values).toStrictEqual([2, 3, 4, 5])
+  expect(series4.sort("ascending").values).toStrictEqual([2, 3, 4, 5])
+  expect(series4.sort(false).values).toStrictEqual([5, 4, 3, 2])
+  expect(series4.sort("descending").values).toStrictEqual([5, 4, 3, 2])
+})
+
+test("throws an error when attempting to do unsavory things with Series", () => {
+  const x = new Series(normal(100))
+
+  expect(() => {
+    x.get("foobar")
+  }).toThrow()
+
+  expect(() => {
+    x.get(-1)
+  }).toThrow()
+})
