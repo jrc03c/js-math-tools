2132d03cd1cafded316ba10c027bea742dd17108
jrc03c <jrc03c@pm.me>
2021-05-30 09:57:41 -0500

    ...

---
+++ src/intersect.js
@@ -0,0 +1,27 @@
+const isArray = require("./is-array.js")
+const flatten = require("./flatten.js")
+const union = require("./union.js")
+
+function intersect() {
+  const arrays = Object.values(arguments).map(v => {
+    if (isArray(v)) return flatten(v)
+    return [v]
+  })
+
+  const out = []
+  const allValues = union(arrays)
+
+  allValues.forEach(value => {
+    for (let i = 0; i < arrays.length; i++) {
+      if (arrays[i].indexOf(value) < 0) {
+        return
+      }
+    }
+
+    out.push(value)
+  })
+
+  return out
+}
+
+module.exports = intersect
