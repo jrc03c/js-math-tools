d688fa5f8a79b88e20e48620d198d5fe38f236ed
jrc03c <jrc03c@pm.me>
2021-10-04 18:45:39 -0500

    Tried speeding up the  function.

--- src/normal.js
+++ src/normal.js
@@ -2,16 +2,29 @@ const isUndefined = require("./is-undefined.js")
 const ndarray = require("./ndarray.js")
 const apply = require("./apply.js")
 const { random } = require("./random.js")
+const reshape = require("./reshape.js")
+const isNumber = require("./is-number.js")
+
+function helper() {
+  const u1 = random()
+  const u2 = random()
+  return Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2)
+}
+
+function product(vals) {
+  let out = 1
+  vals.forEach(v => (out *= v))
+  return out
+}
 
 function normal(shape) {
-  function n() {
-    const u1 = random()
-    const u2 = random()
-    return Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2)
-  }
+  if (isUndefined(shape)) return helper()
 
-  if (isUndefined(shape)) return n()
-  return apply(ndarray(shape), n)
+  if (isNumber(shape)) shape = [shape]
+  const out = []
+  const n = product(shape)
+  for (let i = 0; i < n; i++) out.push(helper())
+  return reshape(out, shape)
 }
 
 module.exports = normal
