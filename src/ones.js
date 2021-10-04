04052526021f00b097a898d2dd3cfa7f0486802d
jrc03c <jrc03c@pm.me>
2021-10-04 18:57:04 -0500

    Tried speeding up the `ones` function.

--- src/ones.js
+++ src/ones.js
@@ -1,8 +1,15 @@
 const ndarray = require("./ndarray.js")
 const apply = require("./apply.js")
+const reshape = require("./reshape.js")
+const isNumber = require("./is-number.js")
 
 function ones(shape) {
-  return apply(ndarray(shape), v => 1)
+  if (isNumber(shape)) shape = [shape]
+  const out = []
+  let n = 1
+  shape.forEach(v => (n *= v))
+  for (let i = 0; i < n; i++) out.push(1)
+  return reshape(out, shape)
 }
 
 module.exports = ones
