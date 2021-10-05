89aa3d7b45bee67cc85f72c5b52661e53e817259
jrc03c <jrc03c@pm.me>
2021-10-04 22:35:40 -0500

    Tried speeding up the `cohensd` function.

--- src/cohens-d.js
+++ src/cohens-d.js
@@ -1,15 +1,11 @@
 const mean = require("./mean.js")
-const sqrt = require("./sqrt.js")
 const variance = require("./variance.js")
-const dropNaNPairwise = require("./drop-nan-pairwise.js")
 
 function cohensd(arr1, arr2) {
   try {
-    const [arr1Temp, arr2Temp] = dropNaNPairwise(arr1, arr2)
-    if (arr1Temp.length === 0 || arr2Temp.length === 0) return NaN
-    const m1 = mean(arr1Temp)
-    const m2 = mean(arr2Temp)
-    const s = sqrt((variance(arr1Temp) + variance(arr2Temp)) / 2)
+    const m1 = mean(arr1)
+    const m2 = mean(arr2)
+    const s = Math.sqrt((variance(arr1) + variance(arr2)) / 2)
     return (m1 - m2) / s
   } catch (e) {
     return NaN
