0312e55234d85615fd342199e00c8c872b594a91
jrc03c <jrc03c@pm.me>
2022-02-09 20:03:23 -0600

    ...

--- src/series/index.js
+++ src/series/index.js
@@ -1,7 +1,6 @@
 const assert = require("../assert.js")
 const copy = require("../copy.js")
 const isArray = require("../is-array.js")
-const isBoolean = require("../is-boolean.js")
 const isFunction = require("../is-function.js")
 const isNumber = require("../is-number.js")
 const isString = require("../is-string.js")
@@ -12,11 +11,10 @@ const ndarray = require("../ndarray.js")
 const range = require("../range.js")
 const reverse = require("../reverse.js")
 const seriesFilter = require("./series-filter.js")
+const seriesSort = require("./series-sort.js")
 const seriesSortByIndex = require("./series-sort-by-index.js")
 const set = require("../set.js")
 const shape = require("../shape.js")
-const sort = require("../sort.js")
-const transpose = require("../transpose.js")
 
 class Series {
   constructor(data) {
@@ -370,47 +368,8 @@ class Series {
   }
 
   sort(direction) {
-    assert(
-      isBoolean(direction) || isString(direction) || isUndefined(direction),
-      "The `sort` method can take an optional parameter that's either a string representing a direction ('ascending' or 'descending') or a boolean representing whether or not the direction is ascending (true or false)."
-    )
-
-    let isAscending = true
-
-    if (isUndefined(direction)) {
-      isAscending = true
-    }
-
-    if (isString(direction)) {
-      direction = direction.trim().toLowerCase()
-
-      assert(
-        direction === "ascending" || direction === "descending",
-        "The `sort` method can take an optional parameter that's either a string representing a direction ('ascending' or 'descending') or a boolean representing whether or not the direction is ascending (true or false)."
-      )
-
-      isAscending = direction === "ascending"
-    }
-
-    if (isBoolean(direction)) {
-      isAscending = direction
-    }
-
     const self = this
-    let temp = transpose([self.values, self.index])
-
-    temp = transpose(
-      sort(temp, (a, b) => {
-        if (a[0] === b[0]) return 0
-        if (a[0] < b[0]) return isAscending ? -1 : 1
-        if (a[0] > b[0]) return isAscending ? 1 : -1
-      })
-    )
-
-    const out = new Series(temp[0])
-    out.index = temp[1]
-    out.name = self.name
-    return out
+    return seriesSort(Series, self, direction)
   }
 
   sortByIndex() {
