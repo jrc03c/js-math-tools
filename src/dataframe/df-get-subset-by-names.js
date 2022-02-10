7962969d6db090a29b1cffb8fd4df5583d6d5571
jrc03c <jrc03c@pm.me>
2022-02-09 19:36:20 -0600

    ...

---
+++ src/dataframe/df-get-subset-by-names.js
@@ -0,0 +1,89 @@
+const isUndefined = require("../is-undefined.js")
+const isString = require("../is-string.js")
+const isArray = require("../is-array.js")
+const shape = require("../shape.js")
+const assert = require("../assert.js")
+const flatten = require("../flatten.js")
+const Series = require("../series")
+
+function dfGetSubsetByNames(DataFrame, df, rows, cols) {
+  if (isUndefined(rows)) rows = df.index
+  if (isUndefined(cols)) cols = df.columns
+  if (isString(rows)) rows = [rows]
+  if (isString(cols)) cols = [cols]
+
+  assert(
+    isArray(rows) && isArray(cols),
+    "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
+  )
+
+  assert(
+    shape(rows).length === 1 && shape(cols).length === 1,
+    "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
+  )
+
+  assert(
+    rows.length > 0,
+    "The `rows` array must contain at least one row name."
+  )
+
+  assert(
+    cols.length > 0,
+    "The `cols` array must contain at least one column name."
+  )
+
+  rows.forEach(row => {
+    assert(
+      isString(row),
+      "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
+    )
+
+    assert(
+      df.index.indexOf(row) > -1,
+      `The row name "${row}" does not exist in the list of rows.`
+    )
+  })
+
+  cols.forEach(col => {
+    assert(
+      isString(col),
+      "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
+    )
+
+    assert(
+      df.columns.indexOf(col) > -1,
+      `The column name "${col}" does not exist in the list of columns.`
+    )
+  })
+
+  const values = rows.map(row => {
+    return cols.map(col => {
+      return df.values[df.index.indexOf(row)][df.columns.indexOf(col)]
+    })
+  })
+
+  if (rows.length === 1 && cols.length === 1) {
+    return flatten(values)[0]
+  }
+
+  if (rows.length === 1) {
+    const out = new Series(flatten(values))
+    out.name = rows[0]
+    out.index = cols
+    return out
+  }
+
+  if (cols.length === 1) {
+    const out = new Series(flatten(values))
+    out.name = cols[0]
+    out.index = rows
+    return out
+  }
+
+  const out = new DataFrame(values)
+  out.columns = cols
+  out.index = rows
+  return out
+}
+
+module.exports = dfGetSubsetByNames
--- src/dataframe/index.js
+++ src/dataframe/index.js
@@ -41,6 +41,7 @@ const dfAssign = require("./df-assign.js")
 const dfCopy = require("./df-copy.js")
 const dfResetIndex = require("./df-reset-index.js")
 const dfGetSubsetByIndices = require("./df-get-subset-by-indices.js")
+const dfGetSubsetByNames = require("./df-get-subset-by-names.js")
 
 function makeKey(n) {
   const alpha = "abcdefghijklmnopqrstuvwxyz1234567890"
@@ -398,84 +399,7 @@ class DataFrame {
 
   getSubsetByNames(rows, cols) {
     const self = this
-
-    if (isUndefined(rows)) rows = self.index
-    if (isUndefined(cols)) cols = self.columns
-    if (typeof rows === "string") rows = [rows]
-    if (typeof cols === "string") cols = [cols]
-
-    assert(
-      isArray(rows) && isArray(cols),
-      "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
-    )
-
-    assert(
-      shape(rows).length === 1 && shape(cols).length === 1,
-      "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
-    )
-
-    assert(
-      rows.length > 0,
-      "The `rows` array must contain at least one row name."
-    )
-
-    assert(
-      cols.length > 0,
-      "The `cols` array must contain at least one column name."
-    )
-
-    rows.forEach(row => {
-      assert(
-        isString(row),
-        "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
-      )
-
-      assert(
-        self.index.indexOf(row) > -1,
-        `The row name "${row}" does not exist in the list of rows.`
-      )
-    })
-
-    cols.forEach(col => {
-      assert(
-        isString(col),
-        "The `rows` and `cols` parameters must be 1-dimensional arrays of strings."
-      )
-
-      assert(
-        self.columns.indexOf(col) > -1,
-        `The column name "${col}" does not exist in the list of columns.`
-      )
-    })
-
-    const values = rows.map(row => {
-      return cols.map(col => {
-        return self.values[self.index.indexOf(row)][self.columns.indexOf(col)]
-      })
-    })
-
-    if (rows.length === 1 && cols.length === 1) {
-      return flatten(values)[0]
-    }
-
-    if (rows.length === 1) {
-      const out = new Series(flatten(values))
-      out.name = rows[0]
-      out.index = cols
-      return out
-    }
-
-    if (cols.length === 1) {
-      const out = new Series(flatten(values))
-      out.name = cols[0]
-      out.index = rows
-      return out
-    }
-
-    const out = new DataFrame(values)
-    out.columns = cols
-    out.index = rows
-    return out
+    return dfGetSubsetByNames(DataFrame, self, rows, cols)
   }
 
   getSubsetByIndices(rowIndices, colIndices) {
