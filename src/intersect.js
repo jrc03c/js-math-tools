bb8d4b12e48a6bd605d0d4fbab8dfc4d0c1d0eb7
jrc03c <jrc03c@pm.me>
2022-09-07 08:40:18 -0500

    ...

--- src/intersect.js
+++ src/intersect.js
@@ -1,38 +1,29 @@
 const assert = require("./assert.js")
-const flatten = require("./flatten.js")
 const isArray = require("./is-array.js")
 const isDataFrame = require("./is-dataframe.js")
 const isEqual = require("./is-equal.js")
 const isSeries = require("./is-series.js")
-const isUndefined = require("./is-undefined.js")
+const set = require("./set.js")
 
-function intersect(a, b) {
-  if (isDataFrame(a) || isSeries(a)) {
-    return intersect(a.values, b)
-  }
-
-  if (isDataFrame(b) || isSeries(b)) {
-    return intersect(a, b.values)
-  }
-
-  assert(
-    isArray(a) && isArray(b),
-    "The `intersect` function only works on arrays, Series, and DataFrames!"
-  )
-
-  const aTemp = flatten(a)
-  const bTemp = flatten(b)
-  const out = []
-
-  aTemp.forEach(item => {
-    const equivalent = bTemp.find(other => isEqual(other, item))
-
-    if (!isUndefined(equivalent)) {
-      out.push(item)
+function intersect() {
+  const arrays = Object.values(arguments).map(x => {
+    if (isDataFrame(x) || isSeries(x)) {
+      return set(x.values)
     }
+
+    assert(
+      isArray(x),
+      "The `intersect` function only works on arrays, Series, and DataFrames!"
+    )
+
+    return set(x)
   })
 
-  return out
+  const all = set(arrays)
+
+  return all.filter(v => {
+    return arrays.every(arr => arr.findIndex(other => isEqual(other, v)) > -1)
+  })
 }
 
 module.exports = intersect
