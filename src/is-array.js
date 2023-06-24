86e48c05478225acbd7829f719022a8589832d5c
jrc03c <jrc03c@pm.me>
2023-06-24 13:34:11 -0500

    ...

--- src/is-array.js
+++ src/is-array.js
@@ -1,10 +1,37 @@
+const isUndefined = require("./is-undefined")
+
+const types = [
+  Array,
+  ArrayBuffer,
+  BigInt64Array,
+  BigUint64Array,
+  Float32Array,
+  Float64Array,
+  Int16Array,
+  Int32Array,
+  Int8Array,
+  Uint16Array,
+  Uint32Array,
+  Uint8Array,
+  Uint8ClampedArray,
+]
+
+const typeStrings = types.map(s => s.name)
+
 function isArray(obj) {
   try {
-    return (
-      obj instanceof Array ||
-      (typeof obj.constructor !== "undefined" &&
-        obj.constructor.name === "Array")
-    )
+    if (obj instanceof Array) {
+      return true
+    }
+
+    if (!isUndefined(obj.constructor)) {
+      return (
+        types.indexOf(obj.constructor) > -1 ||
+        typeStrings.indexOf(obj.constructor.name) > -1
+      )
+    }
+
+    return false
   } catch (e) {
     return false
   }
--- src/is-array.test.js
+++ src/is-array.test.js
@@ -51,3 +51,27 @@ test("tests that arrays can be identified correctly", () => {
     expect(isArray(item)).toBe(false)
   })
 })
+
+test("tests that typed arrays are also recognized as arrays", () => {
+  const types = [
+    BigInt64Array,
+    BigUint64Array,
+    Float32Array,
+    Float64Array,
+    Int16Array,
+    Int32Array,
+    Int8Array,
+    Uint16Array,
+    Uint32Array,
+    Uint8Array,
+    Uint8ClampedArray,
+  ]
+
+  const buffer = new ArrayBuffer(256)
+  expect(isArray(buffer)).toBe(true)
+
+  types.forEach(T => {
+    const x = new T(buffer)
+    expect(isArray(x)).toBe(true)
+  })
+})
